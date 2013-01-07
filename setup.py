bfc11548d04370008a55b3ad4c1442ea146af606
George Katsitadze <george@janrain.com>
2013-01-07 15:25:50 -0800

    Added license, bumped version to 0.2, pointed setup.py to the janrain repo.

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(name='backplane2-pyclient',
-      version='0.1',
+      version='0.2',
       description='Backplane 2 library for python',
       author='George Katsitadze',
       author_email='george@janrain.com',
-      url='http://github.com/geokat/backplane2-pyclient',
+      url='http://github.com/janrain/backplane2-pyclient',
+      license='Apache License 2.0',
       packages = find_packages(),
       install_requires = ['requests'],
       keywords= 'backplane library',
